61261eaa6d7873f2e7a95e229d9fceedf15a1ae7
rishiwalanjkar <rishi.walanjkar@gmail.com>
2013-12-04 03:43:00 +0530

    4/12/13 3:42 AM features done

--- Titan-Reta-x/src/navigation/navigationdrawer/FeaturesFragment.java
+++ Titan-Reta-x/src/navigation/navigationdrawer/FeaturesFragment.java
@@ -2,15 +2,19 @@ package navigation.navigationdrawer;
 
 
 
+import java.util.ArrayList;
+
+import file.reader.ContentReader;
 import zigtraka.nfc.reta_x.R;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FeaturesFragment extends Fragment {
-	
+	private TextView textview;
 	public FeaturesFragment(){}
 	
 	@Override
@@ -19,6 +23,14 @@ public class FeaturesFragment extends Fragment {
  
         View rootView = inflater.inflate(R.layout.fragment_features, container, false);
          
+        textview=(TextView)rootView.findViewById(R.id.fragment_features_textView);
+        StringBuilder result=new StringBuilder();
+        
+        ArrayList<String> features=ContentReader.getToFromContents("$FeatureContent=");
+        for(int i=0;i<features.size();i++)        	
+        result.append("* "+features.get(i).toString()+"\n\n");
+        
+        textview.setText(result.toString());
         return rootView;
     }
 }
